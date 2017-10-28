7007c48c158e6cbf3686f9442e09986bc17e1ae1
Sergey Popinevskiy <gurza000@gmail.com>
2017-10-28 13:44:28 +0300

    1. Refactoring. Dropped redundant properties. 2. Added classifiers to identify project. 3. Described `extras_reuire` for `pip install -e .[dev,test]`.

--- setup.py
+++ setup.py
@@ -17,15 +17,25 @@ setup(
     version=about['__version__'],
     description=about['__description__'],
     long_description=about['__description__'],
+    url=about['__url__'],
     author=about['__author__'],
     author_email=about['__author_email__'],
-    url=about['__url__'],
-    packages=packages,
-    package_data={
-        'redbrick': ['LICENSE'],
-    },
-    include_package_data=True,
     license=about['__license__'],
-    zip_safe=False,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: API clients',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=packages,
     install_requires=requires,
+    extras_require={
+        'test': ['pytest'],
+    },
 )
