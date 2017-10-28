dccf744631728c2a105514e1a4cd940a718867e3
Sergey Popinevskiy <gurza000@gmail.com>
2017-10-28 12:42:37 +0300

    Inited setup.py

---
+++ redbrick/__version__.py
@@ -0,0 +1,7 @@
+__title__ = 'redbrick'
+__description__ = ''
+__url__ = 'https://github.com/Hack-The-Travel/redbrick'
+__version__ = '0.0.0'
+__author__ = 'Sergey Popinevskiy'
+__author_email__ = 'sergey.popinevskiy@gmail.com'
+__license__ = ''
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from codecs import open
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+packages = ['redbrick']
+
+requires = list()
+
+about = dict()
+with open(os.path.join(here, 'redbrick', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    description=about['__description__'],
+    long_description=about['__description__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    license=about['__license__'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+)
