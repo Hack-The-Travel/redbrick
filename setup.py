0199f2670cede2dae7a92958709e8c251a861d7e
Sergey Popinevskiy <gurza000@gmail.com>
2017-10-28 14:19:19 +0300

    Added 'upload' functionality

--- setup.py
+++ setup.py
@@ -1,23 +1,56 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
-from os import path
+from setuptools import setup, Command
+import os
+import sys
 from codecs import open
+from shutil import rmtree
 
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
 packages = ['redbrick']
 
 requires = list()
 
 about = dict()
-with open(path.join(here, 'redbrick', '__version__.py'), 'r', 'utf-8') as f:
+with open(os.path.join(here, 'redbrick', '__version__.py'), 'r', 'utf-8') as f:
     exec(f.read(), about)
 
 with open('README.rst', 'r', 'utf-8') as f:
     readme = f.read()
 with open('HISTORY.rst', 'r', 'utf-8') as f:
     history = f.read()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+        sys.exit()
+
 
 setup(
     name=about['__title__'],
@@ -42,4 +75,7 @@ setup(
     ],
     packages=packages,
     install_requires=requires,
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
