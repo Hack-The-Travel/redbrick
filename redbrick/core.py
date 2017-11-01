da0dfa57cac8e2739ed5672022ef97b25b3865b5
Sergey Popinevskiy <gurza000@gmail.com>
2017-11-01 15:58:41 +0300

    Inited `ClientBrick` class

---
+++ redbrick/core.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+class ClientBrick(object):
+
+    def __init__(self):
+
+        #: Content of the request, in unicode
+        self.last_sent
+
+        #: Content of the response, in unicode
+        self.last_receive
