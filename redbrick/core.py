70ac406d0444a0a89f534290971b5a0472ac276e
Sergey Popinevskiy <gurza000@gmail.com>
2017-11-02 11:41:05 +0300

    Implemented basic logic to dump text of request, response

--- redbrick/core.py
+++ redbrick/core.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
+from datetime import datetime
+import logging
 import requests
+from .utils import dump_to_file
+
+log = logging.getLogger(__name__)
 
 
 class ClientBrick(object):
@@ -14,6 +20,25 @@ class ClientBrick(object):
 
         #: SSL Verification default
         self.verify = True
+
+        #: Path to log files
+        self.log_dir = '/tmp'
+
+    def dump(self, service, fmt, encoding='utf-8'):
+        """Dumps content of last request and response.
+
+        :param encoding: (optional) str, encoding of dump file.
+        """
+        for text, action in [(self.last_sent, 'RQ'), (self.last_receive, 'RS')]:
+            now = datetime.now().strftime('%Y-%m-%dT%H%M%S.%f')
+            path_to_file = os.path.join(
+                self.log_dir,
+                '{dt}_{srv}_{act}.{fmt}'.format(dt=now, srv=service, act=action, fmt=fmt)
+            )
+            try:
+                dump_to_file(path_to_file, text, encoding=encoding)
+            except Exception as e:
+                log.error('Dump error - {}'.format(path_to_file), exc_info=True)
 
     def send(self, method, url):
         """Sends request.
