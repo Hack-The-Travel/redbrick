aefd5e5f695f1c06f11330315f7d0efbd24f7df5
Sergey Popinevskiy <gurza000@gmail.com>
2017-11-02 17:11:59 +0300

    Added exception message. Now it's more clearer.

--- tests/utils.py
+++ tests/utils.py
@@ -2,12 +2,12 @@
 def xstr(s):
     """Converts None to empty string.
 
-    :param s: str or unicode, input string.
+    :param s: None, str or unicode, input string.
     :return: empty string instead of None or a given string.
     :rtype: basestring
     """
     if s is None:
         return ''
     if not isinstance(s, basestring):
-        raise Exception
+        raise TypeError('Input parameter should be None, str or unicode')
     return s
