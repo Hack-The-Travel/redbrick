191daf8d52edd3910c8fe9ac8308fc54d68414a6
Sergey Popinevskiy <gurza000@gmail.com>
2017-11-13 10:56:18 +0300

    Basic auth (#4)
    Supported basic auth.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
+import json
 from redbrick import ClientBrick
 from .utils import xstr
 
@@ -14,3 +15,13 @@ class TestCore:
             assert fd.read() == xstr(client.last_sent)
         with open(dumps[1]) as fd:
             assert fd.read() == xstr(client.last_receive)
+
+    def test_request_basic_auth(self, httpbin):
+        auth = ('user', 'password')
+        url = httpbin.url + '/basic-auth/user/password'
+        client = ClientBrick()
+        with pytest.raises(IOError):
+            client.request('GET', url)
+        client.request('GET', url, auth=auth)
+        response = json.loads(client.last_receive)
+        assert response['authenticated']
