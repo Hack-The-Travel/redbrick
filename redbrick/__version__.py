888619773146480d4e864693ab5b9171021cb9ea
Sergei Popinevskii <gurza000@gmail.com>
2022-11-06 18:07:12 +0300

    up version

--- redbrick/__version__.py
+++ redbrick/__version__.py
@@ -7,7 +7,7 @@
 __title__ = 'redbrick'
 __description__ = ''
 __url__ = 'https://github.com/Hack-The-Travel/redbrick'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'Sergey Popinevskiy'
 __author_email__ = 'sergey.popinevskiy@gmail.com'
 __license__ = 'Apache 2.0'
